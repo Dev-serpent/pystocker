9403a6b6ce5f077dcc0564e5f2adcf1e08093c48
Wakanda Gamasis <129169589+Wakanda-Gamsis@users.noreply.github.com>
2025-09-10 23:53:25 +0530

    Pystocker - Made

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pystocker',
-    version='0.1.0',
-    description='Lightweight Moneycontrol stock data fetcher and analyzer',
-    author='You',
+    name="pystocker",
+    version="0.1.0",
+    author="Wakanda Gamasis - Dev Serpent Team DIV-A",
+    author_email="devserpent.org@gmail.com",
+    description="A stock data mining and analysis library (Moneycontrol scraper)",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dev-serpent/pystocker",
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'beautifulsoup4',
-        'pandas',
-        'matplotlib',
-        'numpy'
+        "requests",
+        "beautifulsoup4",
+        "pandas",
+        "matplotlib",
+        "mplfinance",
     ],
-    python_requires='>=3.8'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
